f96d51ca71379d559f600ee091f55dd42030d79f
sangwoolee <a26484638@gmail.com>
2023-06-27 15:34:16 +0900

    main page

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const newsRouter = require("./routes/news.js");
+const homeRouter = require("./routes/home.route");
 const app = express();
 const port = 3018;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/", newsRouter);
+app.use("/", [newsRouter, homeRouter]);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
---
+++ routes/home.route.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const { User, News } = require("../models");
+const authMiddleware = require("../middlewares/auth-middleware");
+const router = express.Router();
+
+// 회원가입 API
+router.post("/users", async (req, res) => {
+  const { id, password, name, nickName, img } = req.body;
+  const isExistUser = await User.findOne({ where: { id } });
+
+  if (isExistUser) {
+    return res.status(409).json({ message: "이미 존재하는 아이디입니다." });
+  }
+
+  // Users 테이블에 사용자를 추가합니다.
+  const user = await User.create({ id, password, name, nickName, img });
+
+  return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+});
+
+// 로그인 버튼 API
+router.post("/login", async (req, res) => {
+  // id와 pw를 body로 입력받음
+  const { id, password } = req.body;
+  // id로 일치하는 유저 검색
+  const user = await User.findOne({ where: { id } });
+  if (!user) {
+    return res.status(401).json({ message: "존재하지 않는 아이디입니다." });
+  } else if (user.password !== password) {
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+  }
+  // jwt 생성
+  const token = jwt.sign(
+    {
+      userId: user.userId, // userId 할당
+    },
+    "customized_secret_key"
+  );
+  // 쿠키 발급
+  res.cookie("authorization", `Bearer ${token}`);
+  return res.status(200).json({ message: "로그인 성공" });
+});
+
+// 로그아웃 API
+router.get("/logout", authMiddleware, async (req, res) => {
+  // 유저 정보 할당
+  const { userId, id, password, nickName, message } = res.locals.user;
+});
+
+// 뉴스 카드 출력
+router.get("/", async (req, res) => {
+  const newsList = await News.findAll({
+    attributes: [
+      "newsId",
+      "UserId",
+      "title",
+      "content",
+      "img",
+      "category",
+      "createdAt",
+      "updatedAt",
+    ],
+    order: [["createdAt", "DESC"]],
+  });
+
+  return res.status(200).json({ data: newsList });
+});
+
+// 카테고리 정렬
+
+// 좋아요순 정렬
+
+module.exports = router;
